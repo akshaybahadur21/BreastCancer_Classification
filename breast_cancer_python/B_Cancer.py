f1ccae32cb99f3df5e901dd1bb5350071708f79d
Akshay Bahadur <akshay.dominator@gmail.com>
2018-02-24 11:55:45 +0530

    Added support for confusion matrix

--- breast_cancer_python/B_Cancer.py
+++ breast_cancer_python/B_Cancer.py
@@ -4,21 +4,22 @@ import pandas as pd
 
 
 def __init__():
-    train_x=pd.read_csv("cancer_data.csv")
+    train_x = pd.read_csv("cancer_data.csv")
     train_x = np.array(train_x)
-    train_y=pd.read_csv("cancer_data_y.csv")
+    train_y = pd.read_csv("cancer_data_y.csv")
     train_y = np.array(train_y)
-#0.00002-92
-#0.000005-92, 93 when 200000
+    # 0.00002-92
+    # 0.000005-92, 93 when 200000 190500
 
-    d = model(train_x.T, train_y.T, num_iters=190500 , alpha=0.000000065, print_cost=True)
+    d = model(train_x.T, train_y.T, num_iters=190500, alpha=0.000000065, print_cost=True)
+
 
 # Logistic Regression as deep learning
 
 
 
 def sigmoid(z):
-    s = 1/(1+np.exp(-z))
+    s = 1 / (1 + np.exp(-z))
     return s
 
 
@@ -30,7 +31,7 @@ def initialize(dim):
     """
     w = np.zeros(shape=(dim, 1))
     b = 0
-    return w,   b
+    return w, b
 
 
 def propagate(w, b, X, Y):
@@ -42,23 +43,23 @@ def propagate(w, b, X, Y):
     :param Y: true label
     :return:
     """
-    m=X.shape[1] #getting no of rows
+    m = X.shape[1]  # getting no of rows
 
-
-    #Forward Prop
-    A=sigmoid(np.dot(w.T,X)+b)
+    # Forward Prop
+    A = sigmoid(np.dot(w.T, X) + b)
     cost = (-1 / m) * np.sum(Y * np.log(A) + (1 - Y) * (np.log(1 - A)))
 
-    #backwar prop
+    # backwar prop
     dw = (1 / m) * np.dot(X, (A - Y).T)
     db = (1 / m) * np.sum(A - Y)
 
     cost = np.squeeze(cost)
-    grads={"dw":dw,
-           "db":db}
-    return grads,cost
+    grads = {"dw": dw,
+             "db": db}
+    return grads, cost
 
-def optimize(w,b,X,Y,num_iters,alpha,print_cost=False):
+
+def optimize(w, b, X, Y, num_iters, alpha, print_cost=False):
     """
 
     :param w: weights for w
@@ -70,13 +71,13 @@ def optimize(w,b,X,Y,num_iters,alpha,print_cost=False):
     :return:
     """
 
-    costs=[]
+    costs = []
     for i in range(num_iters):
-        grads, cost=propagate(w,b,X,Y)
-        dw=grads["dw"]
-        db=grads["db"]
-        w=w-alpha*dw
-        b=b-alpha*db
+        grads, cost = propagate(w, b, X, Y)
+        dw = grads["dw"]
+        db = grads["db"]
+        w = w - alpha * dw
+        b = b - alpha * db
 
         # Record the costs
         if i % 100 == 0:
@@ -87,14 +88,15 @@ def optimize(w,b,X,Y,num_iters,alpha,print_cost=False):
             print("Cost after iteration %i: %f" % (i, cost))
 
     params = {"w": w,
-                  "b": b}
+              "b": b}
 
     grads = {"dw": dw,
-                 "db": db}
+             "db": db}
 
     return params, grads, costs
 
-def predict(w,b,X):
+
+def predict(w, b, X):
     """
 
     :param w:
@@ -102,16 +104,17 @@ def predict(w,b,X):
     :param X:
     :return:
     """
-    m=X.shape[1]
-    y_pred=np.zeros(shape=(1,m))
-    w=w.reshape(X.shape[0],1)
+    m = X.shape[1]
+    y_pred = np.zeros(shape=(1, m))
+    w = w.reshape(X.shape[0], 1)
 
-    A=sigmoid(np.dot(w.T,X)+b)
+    A = sigmoid(np.dot(w.T, X) + b)
+
 
     for i in range(A.shape[1]):
-        y_pred[0,i]=1 if A[0,i]>0.5 else 0
+        y_pred[0, i] = 1 if A[0, i] > 0.5 else 0
 
-    assert (y_pred.shape==(1,m))
+    assert (y_pred.shape == (1, m))
     return y_pred
 
 
@@ -128,22 +131,66 @@ def model(X_train, Y_train, num_iters=2000, alpha=0.5, print_cost=False):
     :return:
     """
 
-    w,b=initialize(X_train.shape[0])
-    parameters,grads,costs=optimize(w,b,X_train,Y_train,num_iters,alpha,print_cost)
+    w, b = initialize(X_train.shape[0])
+    parameters, grads, costs = optimize(w, b, X_train, Y_train, num_iters, alpha, print_cost)
 
-    w=parameters["w"]
-    b=parameters["b"]
+    w = parameters["w"]
+    b = parameters["b"]
 
-    X_test=pd.read_csv("test_cancer_data.csv")
-    X_test=np.array(X_test)
-    X_test=X_test.T
-    Y_test=pd.read_csv("test_cancer_data_y.csv")
-    Y_test=np.array(Y_test)
-    Y_test=Y_test.T
-    y_prediction_train=predict(w,b,X_train)
-    y_prediction_test=predict(w,b,X_test)
+    X_test = pd.read_csv("test_cancer_data.csv")
+    X_test = np.array(X_test)
+    X_test = X_test.T
+    Y_test = pd.read_csv("test_cancer_data_y.csv")
+    Y_test = np.array(Y_test)
+    Y_test = Y_test.T
+    truePositive = 0
+    trueNegative = 0
+    falseNegative = 0
+    falsePositive = 0
+    y_prediction_train = predict(w, b, X_train)
+    y_prediction_test = predict(w, b, X_test)
     print("train accuracy: {} %".format(100 - np.mean(np.abs(y_prediction_train - Y_train)) * 100))
+    predList = y_prediction_train.tolist()
+    tlist = Y_train.tolist()
+
+    array_length = len(predList[0])
+    for i in range(array_length):
+        if predList[0][i] == 1 & tlist[0][i] == 1:
+            truePositive += 1
+        elif predList[0][i] == 0 & tlist[0][i] == 0:
+            trueNegative += 1
+        elif predList[0][i] == 0 & tlist[0][i] == 1:
+            falseNegative += 1
+        else:
+            falsePositive += 1
+    print("On training set:\nTrue Positive:  ", truePositive)
+    print("True Negative:  ", trueNegative)
+    print("False Negative:  ", falseNegative)
+    print("False Positive:  ", falsePositive)
+
     print("test accuracy: {} %".format(100 - np.mean(np.abs(y_prediction_test - Y_test)) * 100))
+
+    truePositive = 0
+    trueNegative = 0
+    falseNegative = 0
+    falsePositive = 0
+    predList = y_prediction_test.tolist()
+    tlist = Y_test.tolist()
+
+    array_length = len(predList[0])
+    for i in range(array_length):
+        if predList[0][i] == 1 & tlist[0][i] == 1:
+            truePositive += 1
+        elif predList[0][i] == 0 & tlist[0][i] == 0:
+            trueNegative += 1
+        elif predList[0][i] == 0 & tlist[0][i] == 1:
+            falseNegative += 1
+        else:
+            falsePositive += 1
+    print("On Test set:\nTrue Positive:  ", truePositive)
+    print("True Negative:  ", trueNegative)
+    print("False Negative:  ", falseNegative)
+    print("False Positive:  ", falsePositive)
     d = {"costs": costs,
          "Y_prediction_test": y_prediction_test,
          "Y_prediction_train": y_prediction_train,
@@ -164,4 +211,3 @@ def model(X_train, Y_train, num_iters=2000, alpha=0.5, print_cost=False):
 
 
 __init__()
-
