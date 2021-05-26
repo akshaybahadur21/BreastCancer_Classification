ff154ee6ba38637a035f75dabbda8449ac587ea6
Divya Lalwani <68968821+divyalalwani@users.noreply.github.com>
2021-05-26 19:33:31 +0530

    Update plotData.m

--- breast_cancer/plotData.m
+++ breast_cancer/plotData.m
@@ -1,4 +1,4 @@
-function plotData(X, y)
+function plotData(x, y)
 %PLOTDATA Plots the data points X and y into a new figure 
 %   PLOTDATA(x,y) plots the data points with + for the positive examples
 %   and o for the negative examples. X is assumed to be a Mx2 matrix.
